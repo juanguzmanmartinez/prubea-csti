9b0922879838c0f2ab7a518529dd89db16164c6e
Juan Guzman <soluciondigital16@farmaciasperuanas.pe>
2023-12-26 20:29:03 -0500

    feat:add pwa

--- vite.config.js
+++ vite.config.js
@@ -9,35 +9,29 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      mode: "development",
-      base: "/",
-      srcDir: "src",
-      filename: "sw.ts",
-      includeAssets: ["/favicon.png"],
-      strategies: "injectManifest",
       manifest: {
-        name: "Test Project",
-        short_name: "Test",
-        theme_color: "#ffffff",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
         icons: [
           {
-            src: "icon-192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icon-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "icon-512.png",
+            src: "/icons/512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              return url.pathname.startsWith("/api");
+            },
+            handler: "CacheFirst",
+            options: {
+              cacheName: "api-cache",
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
           },
         ],
       },
