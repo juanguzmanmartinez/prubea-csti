3272aff4fcdbcf646b4d5a4f338bb7c561f283b7
Juan Guzman <soluciondigital16@farmaciasperuanas.pe>
2023-12-27 10:56:46 -0500

    feat:add manifest

--- vite.config.js
+++ vite.config.js
@@ -10,12 +10,33 @@ export default defineConfig({
     vue(),
     VitePWA({
       manifest: {
+        theme_color: "#212fb0",
+        background_color: "#4926a9",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        name: "prueba-csti",
+        short_name: "csti",
         icons: [
           {
-            src: "/icons/512.png",
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any maskable",
           },
         ],
       },
